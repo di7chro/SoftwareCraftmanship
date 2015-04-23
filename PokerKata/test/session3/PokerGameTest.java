a19f8640fd049541b1dd90e7a0e0074e7043997f
di7chro <crille@crille.org>
2015-04-23 09:48:20 +0200

    Lite fler test

--- PokerKata/test/session3/PokerGameTest.java
+++ PokerKata/test/session3/PokerGameTest.java
@@ -1,10 +1,13 @@
 package session3;
 
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class PokerGameTest {
-	// FIXME give_me_five
+	// FIXME get_color
+	// FIXME get_value
 	// FIXME draw_one_card
 	// FIXME draw_five_cards
 	// FIXME draw_one_hand
@@ -12,13 +15,36 @@ public class PokerGameTest {
 	// FIXME find_highest_score
 
 	PokerGame myGame = new PokerGame();
-	
+
 	@Test
 	public void give_me_five() throws Exception {
+
+		int result = myGame.getHand();
+
+		assertEquals(5, result);
+	}
+
+	@Test
+	@Ignore
+	public void get_color() throws Exception {
+		int result = myGame.getHand();
 		
+		assertEquals("Spades", result);
+	}
+
+	@Test
+	@Ignore
+	public void get_value() throws Exception {
 		int result = myGame.getHand();
 		
 		assertEquals(5, result);
 	}
 
+	@Test
+	@Ignore
+	public void draw_one_card() throws Exception {
+		int result = myGame.getHand();
+
+		assertEquals("C7", result);
+	}
 }
